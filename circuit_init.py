c1557cb0f0d682ce1681b4a48d19fe875710ab5a
chrischris96 <christian.plitz@gmx.de>
2020-06-21 14:08:36 +0200

    update initial circuit + VQE
    Usage of the variational quantum eigensolver for computational quantum chemical calculations by the IBM qiskit framework. Works as base for dimensionality reduction in hilbert space for more effient (less qubits used) approach

--- circuit_init.py
+++ circuit_init.py
@@ -1,18 +1,45 @@
-from qiskit import QuantumCircuit, BasicAer, execute
-from qiskit.visualization import plot_bloch_multivector
+from qiskit import QuantumCircuit, Aer, execute
+from qiskit.visualization import plot_histogram
 
-circ = QuantumCircuit(10,10)
+# instructions of the quantum system
+# initializing with 2 qubits in the zero state; 
+# with 2 classical bits set to zero;
+circ = QuantumCircuit(3,3)
 
-for idx in range(10):
+for idx in range(3):
    circ.h(idx)
 
 
+
 circ.draw()
+
+# Create a Quantum Circuit
+meas = QuantumCircuit(3, 3)
+# map the quantum measurement to the classical bits
+meas.measure(range(3), range(3))
+
+# The Qiskit circuit object supports composition using
+# the addition operator.
+qc = circ+meas
+
+#drawing the circuit
+qc.draw()
+
 # calculation of all single-qubit gates on all the qubits happen simultaneously, the number of qubits in this circuit is equal to the width of the circuit
 print(circ.width())
 print(circ.depth())
 
 
-backend = BasicAer.get_backend('statevector_simulator')
-job = execute(circ, backend).result()
-plot_bloch_multivector(job.get_statevector(), title="New Bloch Multivector")
+backend = Aer.get_backend('statevector_simulator')
+job = execute(circ, backend)
+result = job.result()
+outputstate = result.get_statevector(circ, decimals=3)
+
+# Show the results
+print(outputstate)
+
+job_exp = execute(qc, backend=backend)
+result_exp = job_exp.result()
+counts_exp = result_exp.get_counts(circ)
+
+plot_histogram(circ)
